8292b42f4b75f7a2fd36b9eff4ddf4236a16ebf9
Alessia Visconti <alessia.visconti@gmail.com>
2019-10-25 17:15:30 +0100

    Added GWAS Catalog functions

---
+++ R/query_GWAS_Catalog.R
@@ -0,0 +1,82 @@
+GWAS.catalog.SNP <- function(rs)
+#' Fetches associations in the GWAS Catalog involving the gived SNP
+#'
+#' Uses GWAS Catalog Access via API (\url{https://www.ebi.ac.uk/gwas/rest/docs/api}).
+#' to fetch GWAS associations. If no associations is found, NULL is returned.
+#' 
+#' @author Alessia Visconti
+#' @param rs reference SNP IDs for the variant
+#' @return a data frame listing the associations recorded in the GWAS catalog
+#' @examples
+#' GWAS.catalog.SNP("rs7681615")
+#' GWAS.catalog.SNP("rs4693052")
+#' @export
+{
+	#API returns JSON that I need to parse
+	url <- paste0("https://www.ebi.ac.uk/gwas/rest/api/associations/search/findByRsId?rsId=", rs)
+	association <- jsonlite::fromJSON(txt=url)
+	association <- association[[1]]$association
+	
+	#Checks whether there are any results
+	if (is.null(dim(association))) return(NULL)
+	
+	#Does some formatting and extracts the sub data frames, starting from the
+	#infomation on the SNP (loci)
+	association$pvalueMantissa <- association$pvalueExponent <- NULL
+	loci <- do.call(rbind, lapply(association$loci, function(r) 
+	{
+		strongestRiskAlleles <- r$strongestRiskAlleles
+		strongestRiskAlleles <- lapply(strongestRiskAlleles, function(l) {l$"_links" <- NULL; l})
+		strongestRiskAlleles <- do.call(rbind, strongestRiskAlleles)
+	}))
+	association$loci <- NULL
+		
+	#Then extracts information on the trait and the study (more information are 
+	#available, but I decided to discard them)
+	links <- association$"_links"
+	study <- t(sapply(links$study[, 1], function(url) {
+		study <- jsonlite::fromJSON(txt=url)
+		r <- c(study$diseaseTrait$trait, study$publicationInfo$pubmedId)
+		names(r) <- c("trait", "pubmedId")
+		r
+	}))
+	association$"_links" <- NULL
+	association <- cbind(loci, study, association)
+	rownames(association) <- NULL
+
+	association
+}
+
+
+GWAS.catalog.SNP.proxy <- function(rs, r2=c("r2", "d"), pop="GBR", LDlinktoken, min.r2=0.01, max.distance=500000)
+#' Fetches associations in the GWAS Catalog involving the gived all proxy SNP
+#'
+#' Uses LDproxy Programmatic Access via API (\url{https://ldlink.nci.nih.gov/?tab=ldproxy}),
+#' to extract the proxy SNPs, and then uses GWAS Catalog Access via API 
+#' (\url{https://www.ebi.ac.uk/gwas/rest/docs/api}) to fetch the GWAS associations.
+#' If no associations is found, NULL is returned.
+#' 
+#' @author Alessia Visconti
+#' @param rs reference SNP IDs for the variant
+#' @param r2 whether look for r2 or D'
+#' @param pop population (multiple populations should be formatted as pop1\%2pop2\%2pop3)
+#' @param LDlinktoken personal token (request via \url{https://ldlink.nci.nih.gov/?tab=apiaccess})
+#' @param min.r2 minum LD (r2) to return 
+#' @param max.distance maximun distance (in bp) to return 
+#' @return a data frame listing the associations recorded in the GWAS catalog
+#' @seealso LDproxy
+#' @seealso GWAS.catalog.SNP
+#' @examples
+#' GWAS.catalog.SNP.proxy("rs7681615", "r2", "GBR", "n0tw0rk1ng", "0.8", "500000")
+#' GWAS.catalog.SNP.proxy("rs4693052", "r2", "GBR", "n0tw0rk1ng", "0.8", "500000")
+#' @export
+{
+	#Selects proxy SNPs
+	snps <- LDproxy(rs, r2, pop, LDlinktoken, min.r2=min.r2, max.distance=max.distance)
+	
+	if (nrow(snps) == 0) return(NULL)
+	
+	#Gets the associations
+	associations <- lapply(snps$RS_Number, GWAS.catalog.SNP)
+	do.call(rbind, associations) 
+}
--- README.md
+++ README.md
@@ -28,3 +28,13 @@ timesaveR is licensed under GNU GPL v3.
 While some of the functions have been developed 100% by me, others are liberal (or not so liberal) adaptations of other people's work, several of whom I had lost track. I am happy to include attribution whenever I can: if you spot your or someone else's work, please let me know.
 
 
+## Changelog
+
+### 0.0.1.2 / 2019-10-25
+
+Enhancements:
+* Functions `biomart.*.in.window` can now return also the chromosomal coordinates
+* Added a function (`biomart.SNP.position`) that returns the chromosomal coordinates of a given SNP
+* Added functions to query the [GWAS Catalog](https://www.ebi.ac.uk/gwas/home) (`GWAS.catalog.SNP` and `GWAS.catalog.SNP.proxy`)
+
+
